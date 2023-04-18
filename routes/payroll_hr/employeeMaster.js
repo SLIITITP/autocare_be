7ce49004e0138ab0437ff06ce8adfcf8b77bd253
Shamith Rosa <shamithrosa@gmail.com>
2023-04-18 18:12:09 +0530

    emp master

--- routes/payroll_hr/employeeMaster.js
+++ routes/payroll_hr/employeeMaster.js
@@ -2,7 +2,7 @@ let express = require("express");
 let router = express.Router();
 let dbConnection = require("./../../util/db-helper/db_connection");
 
-/*Add an employee*/
+//Add an employee
 router.post("/api/employee/add-employee", (req, res, next) => {
   try {
     let BasicInfo = req.body.BasicInfo;
@@ -14,6 +14,31 @@ router.post("/api/employee/add-employee", (req, res, next) => {
     dbConnection.query(
       sqlQuery,
       [BasicInfo, JobInfo, LeaveInfo, PayrollInfo],
+      (_error, result, fields) => {
+        if (_error) console.error(_error);
+
+        console.log(result);
+        res.json(result);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//update an employee
+router.put("/api/employee/update-employee", (req, res, next) => {
+  try {
+    let EmpID = req.body.EmployeeID;
+    let BasicInfo = req.body.BasicInfo;
+    let JobInfo = req.body.JobInfo;
+    let LeaveInfo = req.body.LeaveInfo;
+    let PayrollInfo = req.body.PayrollInfo;
+
+    let sqlQuery = `call USP_UpdateEmployee(?,?,?,?,?)`;
+    dbConnection.query(
+      sqlQuery,
+      [EmpID, BasicInfo, JobInfo, LeaveInfo, PayrollInfo],
       (_error, result, fields) => {
         if (_error) console.error(_error);
 
