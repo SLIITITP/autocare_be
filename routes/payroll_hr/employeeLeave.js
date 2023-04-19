c1934bc61e15597b525a4e1c4043da5095706059
it21215360 <it21215360@my.sliit.lk>
2023-04-19 16:05:49 +0530

    employeeLeave

--- routes/payroll_hr/employeeLeave.js
+++ routes/payroll_hr/employeeLeave.js
@@ -21,13 +21,13 @@ router.post("/api/employee/leave-request-form", (req, res, next) => {
     }catch (error) {
         console.error(error);
     }
-});
+}); 
 
 //list-leave info data
 router.get("/api/employee/leave-approval", (req, res, next) => {
     try {
       dbConnection.query(
-        "SELECT AutoID, EmployeeID, FirstName, LastName, EmpDepartment, LeaveCategory, LeaveType, DATE_FORMAT(LeaveFrom, '%m/%d/%Y') AS LeaveFrom, DATE_FORMAT(LeaveTo, '%m/%d/%Y') AS LeaveTo, DayCount, Status FROM EmployeeLeaveRequest;",
+        "SELECT AutoID, EmployeeID, FirstName, LastName, Position, LeaveCategory, LeaveType, DATE_FORMAT(LeaveFrom, '%m/%d/%Y') AS LeaveFrom, DATE_FORMAT(LeaveTo, '%m/%d/%Y') AS LeaveTo, DayCount, Status FROM EmployeeLeaveRequest;",
         (_error, result, fields) => {
           if (_error) console.error(_error);
   
@@ -40,6 +40,27 @@ router.get("/api/employee/leave-approval", (req, res, next) => {
     }
   });
   
-
+//update leave-request
+router.put("/api/employee/leave-request-approval", (req, res, next) => {
+    try {
+      let EmpID = req.body.EmployeeID;
+      let LeaveInfo = req.body.LeaveInfo;
+  
+      let sqlQuery = `call USP_UpdateLeaveRequest(?,?)`;
+      dbConnection.query(
+        sqlQuery,
+        [ EmpID,LeaveInfo],
+        (_error, result, fields) => {
+          if (_error) console.error(_error);
+  
+          console.log(result);
+          res.json(result);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  
 
 module.exports = router;
