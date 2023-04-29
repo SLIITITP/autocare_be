d8b0ca2f808a73731e80604a70f46368639c49e7
Fernando M.A.N <it21238994@my.sliit.lk>
2023-04-29 18:30:03 +0530

    raise ticket changes

--- routes/customer_care/raiseTicket.js
+++ routes/customer_care/raiseTicket.js
@@ -4,41 +4,33 @@ let dbConnection = require("../../util/db-helper/db_connection");
 
 //add raise ticket form data
 router.post("/api/customer/raise-ticket-form", (req, res, next) => {
-    try{
-        let TicketInfo = req.body.TicketInfo;
+  try {
+    let TicketInfo = req.body.TicketInfo;
+    console.log(TicketInfo);
+    let sqlQuery = `call USP_AddTicket(?)`;
+    dbConnection.query(sqlQuery, [TicketInfo], (_error, result, fields) => {
+      if (_error) throw _error;
 
-        let sqlQuery = `call USP_AddTicket(?)`;
-        dbConnection.query(
-            sqlQuery,
-            [TicketInfo],
-            (_error, result, fields) => {
-                if (_error) throw _error;
-                
-                console.log(result);
-                res.json(result);
-            }
-        );
-    }catch (error) {
-        console.error(error);
-    }
+      console.log(result);
+      res.json(result);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 //list tickets
 router.get("/api/customer/list-raise-ticket", (req, res, next) => {
-    try {
-      dbConnection.query(
-        "SELECT * FROM TicketInfo",
-        (_error, result, fields) => {
-          if (_error) console.error(_error);
-  
-          console.log(result);
-          res.json(result);
-        }
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  });
+  try {
+    dbConnection.query("SELECT * FROM TicketInfo", (_error, result, fields) => {
+      if (_error) console.error(_error);
 
+      console.log(result);
+      res.json(result);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-  module.exports = router;
+module.exports = router;
--- routes/customer_care/trackTicket.js
+++ routes/customer_care/trackTicket.js
@@ -5,11 +5,13 @@ let dbConnection = require("../../util/db-helper/db_connection");
 //update ticket status
 router.put("/api/employee/track-ticket-status", (req, res, next) => {
     try{
-        let TicketInfo = req.body.TicketInfo;
+        let _TicketID = req.body.TicketID;
+        let _statusValue = req.body.Status;
 
         let sqlQuery = `call USP_UpdateTicket(?)`;
         dbConnection.query(
             sqlQuery,
+            [_TicketID, _statusValue],
             (_error, result, feilds) => {
                 if (_error) console.error(_error);
 
