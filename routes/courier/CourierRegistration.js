d555e14b8348fb97a8e80c89f5f18fbc801bdc45
shenika <it21307362@my.sliit.lk>
2023-05-08 17:53:14 +0530

    Deliveryallocation changes

--- routes/courier/CourierRegistration.js
+++ routes/courier/CourierRegistration.js
@@ -20,3 +20,21 @@ router.post('/api/register/add-register', (req, res, next) => {
 })
 
 module.exports = router;
+
+
+//list request
+router.get("/api/courier/list-courier", (req, res, next) => {
+  try {
+    dbConnection.query(
+      'SELECT * FROM CourierReg',
+      (_error, result, fields) => {
+        if (_error) console.error(_error)
+
+        console.log(result)
+        res.json(result)
+      }
+    )
+  } catch (error) {
+    console.error(error)
+  }
+})
